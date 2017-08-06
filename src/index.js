38f5d02d7bac91b4b3f2447900c91d775d23d7e6
markgreenburg <marek.greenburg@gmail.com>
2017-08-06 12:40:48 -0700

    Added search bar

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+// Import subcomponents
+import SearchBar from './components/searchBar';
+
+// Pull in youtube API key and search lib
+import youtube from '../config.js';
+
+
 // Create new functional component that produces some HTML
-const App = () => <div>Hi!</div>;
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+      Hi!
+    </div>
+  );
+};
 
 // Take component's HTML and put it in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
