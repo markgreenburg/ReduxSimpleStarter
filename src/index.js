9f6dc3163528ecfcd61952f7acd6d73cb1943c06
markgreenburg <marek.greenburg@gmail.com>
2017-08-06 12:11:20 -0700

    Working boilerplate

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-// Create new component that produces some HTML
-const app = () => <div>Hi!</div>;
+// Import react library
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+// Create new functional component that produces some HTML
+const App = () => <div>Hi!</div>;
 
 // Take component's HTML and put it in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
