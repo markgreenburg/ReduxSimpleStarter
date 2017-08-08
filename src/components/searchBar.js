762bc53d1dcf9adb8848a14da05208d540019467
markgreenburg <marek.greenburg@gmail.com>
2017-08-07 23:23:05 -0700

    Added all components
    Added ability to watch selected video, select other videos from list

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,5 +1,28 @@
 import React, { Component } from 'react';
 
-const SearchBar = () => <input />;
+// const SearchBar = () => <input />;
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          onChange={this.onInputChange}
+          value={this.state.term}
+        />
+      </div>
+    )
+  }
+}
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,50 @@
 // Import react library
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 // Import subcomponents
 import SearchBar from './components/searchBar';
+import VideoList from './components/videoList';
+import VideoDetail from './components/videoDetail';
 
 // Pull in youtube API key and search lib
 import youtube from '../config.js';
 
 
-// Create new functional component that produces some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-      Hi!
-    </div>
-  );
+
+// Create new class component that produces some HTML
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+  }
+
+  componentDidMount() {
+    YTSearch({ key: youtube, term: 'surfboards' }, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail
+          video={this.state.selectedVideo}/>
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
+        />
+      </div>
+    );
+  }
 };
 
 // Take component's HTML and put it in the DOM
